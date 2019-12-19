1dde50d8f4523d00256909b5fd1c0bd5f69199f8
Artjoms Vorona <->
2019-12-19 11:42:01 +0200

    Initial Commit

---
+++ HWS_QRscanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWS_QRscanner
+//
+//  Created by Artjoms Vorona on 19/12/2019.
+//  Copyright © 2019 Artjoms Vorona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
